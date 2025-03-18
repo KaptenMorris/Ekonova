0bc651671b638efdd44ed037bf848c448815bdb9
KaptenMorris <m0rphybyte@gmail.com>
2025-03-19 00:10:40 +0100

    Implementera hela layouten

---
+++ app/(root)/betalningsoverforing/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Betalningsoverforing = () => {
+  return (
+    <div>Överföra</div>
+  )
+}
+
+export default Betalningsoverforing
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,38 @@
+import HeaderBox from '@/components/HeaderBox'
+import RigthSidebar from '@/components/RigthSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Hem = () => {
+  const loggIn = { firstName: 'Marius', lastName: 'Christensen' ,email: 'info@marius-chritensen.se'}
+
   return (
-    <div>Hem</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Välkommen"
+            user={loggIn?.firstName || 'Gäst'}
+            subtext="Få tillgång till och hantera ditt konto och dina transaktioner effektivt"
+          />
+
+          <TotalBalanceBox 
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={12560.56}
+          />
+        </header>
+
+        SENASTE TRANSAKTIONER
+      </div>
+
+      <RigthSidebar
+        user={loggIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50},{currentBalance:500.50}]}
+      />
+      </section>
   )
 }
 
